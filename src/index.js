34708dc73387b2667bd368be02d82955ead00444
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-09-06 04:37:30 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+
+class Spaceship extends React.Component {
+  render() {
+    const { name, speed, hasRockets, colors } = this.props;
+
+   import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Spaceship from './components/Spaceship';
 
 ReactDOM.render(
   <Spaceship name="Millennium Falcon" />,
-  document.getElementById('global')
+  document.getElementById('root')
 );
